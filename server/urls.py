d4b018e742e7e4ca0f3069f687db0e151e45a8a4
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-09-22 02:08:47 +0600

    Rename project folder name✨🔨

--- server/urls.py
+++ server/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("admin", admin.site.urls),
-    path("api/product/", include("api.urls.product_urls")),
+    path("api/product/", include("api.urls.product_urls"), name='product'),
     path("api/order/", include("api.urls.order_urls")),
     path("api/user/", include("api.urls.user_urls")),
     # Sitemap Route
